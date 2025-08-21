ece23b7533d3a5987e26b74365d1225cd24c5dc0
Nitin-0017 <raonitinkumar06@gmail.com>
2025-08-21 13:05:04 +0530

    adding files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+
+let ans = fs.readFileSync("input.txt","utf-8")
+
+let data = ans.split("\n")
+
+total = 0
+
+data.forEach((ele)=>{
+    let num = ele.split(" ")[1]
+    total += Number(num)
+})
+
+fs.writeFileSync("output.txt",total)
